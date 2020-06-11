039ca9cbf4e53b6b9ec57c1fc8df4ded4a971353
dmdeklerk <dennis@heatledger.com>
2020-06-11 12:32:26 +0200

    Updates

---
+++ precache-manifest.3f7623f8112ef078e749b1dfbab4f0a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a057974706ed8cb7f7fcef3400636750",
+    "url": "/heat-paper/index.html"
+  },
+  {
+    "revision": "3e3931ec7c5dc0f5df06",
+    "url": "/heat-paper/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3e3931ec7c5dc0f5df06",
+    "url": "/heat-paper/static/js/2.69bb1d91.chunk.js"
+  },
+  {
+    "revision": "870a54fb36dac74a6a825ab1a488d56e",
+    "url": "/heat-paper/static/js/2.69bb1d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87c27533f853188c98de",
+    "url": "/heat-paper/static/js/main.867e16e5.chunk.js"
+  },
+  {
+    "revision": "e0814ef8541443df80e4",
+    "url": "/heat-paper/static/js/runtime-main.32776c20.js"
+  }
+]);
